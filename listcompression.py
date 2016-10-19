c117f4e3762b55ec12e10caa4a23f5a95c633ce1
Maggie Chege <shikooh.maggie@gmail.com>
2016-10-19 12:46:47 +0300

    This program prints even numbers from a list

---
+++ listcompression.py
@@ -0,0 +1,14 @@
+#this program checks variables in a list and separates those that are even from the list
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print a
+print("The following is the list of even Numbers  from the list")
+for v in a:
+    # print v
+   if  v %2 == 0:
+     #  print v
+     #learnt to append these brases to numbers []
+       numo =[]
+       numo.append(v)
+       print numo
+  
+
