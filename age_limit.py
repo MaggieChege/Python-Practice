50a424ee1a650ec5a49703da9112839e459f7029
Maggie Chege <shikooh.maggie@gmail.com>
2016-10-25 14:18:11 +0300

    This program check the age of the person and determines if he's a sponsor or not

---
+++ age_limit.py
@@ -0,0 +1,9 @@
+def sponsor(age):
+    "This function checks the age and determines if he is young or sponsor"
+    retire = age/2 +10
+    return retire
+aged = sponsor(100)
+young = sponsor(26)
+
+print "You are",aged,"years old, leave them girls alone"
+print "You can date chics aged",young,"years old"
